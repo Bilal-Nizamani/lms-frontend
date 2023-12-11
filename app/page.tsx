21809c643d634491a40cf3525365e7d7add60e47
Bilal-Nizamani <bilalnizamani001@gmail.com>
2023-12-11 09:58:58 +0500

    created navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
+import Header from "./components/Header";
 
 interface Props {}
 
 const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
   return (
     <div>
       <Heading
@@ -12,6 +15,8 @@ const Page: FC<Props> = (props) => {
         description="ELearning is platform for students to learn and get help "
         keywords="Programing, MERN, Redux, Machine, Learning"
       />
+
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
     </div>
   );
 };
